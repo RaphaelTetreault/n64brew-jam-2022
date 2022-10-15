f276bebc7cd623db66620e8336c07234f2bfb068
Raphaël Tétreault <raphaeltetreault@hotmail.com>
2022-10-14 23:34:28 -0400

    Add level assets

---
+++ tiled/basic-collision.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="basic-collision" tilewidth="16" tileheight="16" tilecount="4" columns="2">
+ <image source="collision.png" width="32" height="32"/>
+</tileset>
